2feb5666af166c03e11cfd9da8ef9b42a7438b29
ThamizhiniyanCS <csthamizhiniyan@gmail.com>
2025-06-30 22:13:19 +0530

    Rust: difference-of-squares completed

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,14 @@
+// Reference: https://brilliant.org/wiki/sum-of-n-n2-or-n3/
+
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum = n * (n + 1) / 2;
+    sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    n * (n + 1) * (2 * n + 1) / 6
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
