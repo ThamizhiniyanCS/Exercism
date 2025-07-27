946f90e3cdd5aa7c3b60062dbc9779db0ce83edf
ThamizhiniyanCS <95332789+ThamizhiniyanCS@users.noreply.github.com>
2025-07-27 19:53:16 +0530

    Rust: high-scores and matching-brackets completed

--- rust/sum-of-multiples/src/main.rs
+++
@@ -1,9 +0,0 @@
-use sum_of_multiples::*;
-
-fn main() {
-    let factors = &[3];
-    let limit = 7;
-    let output = sum_of_multiples(limit, factors);
-    let expected = 9;
-    assert_eq!(output, expected);
-}
